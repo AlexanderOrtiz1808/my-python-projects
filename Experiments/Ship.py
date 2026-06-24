63f6edc1d7711ed29b76a55774597dac2b14aafd
Alexander Ortiz <ortiz.alexander1808@gmail.com>
2026-06-24 16:19:41 -0600

    nuevo avance

--- Experiments/Ship.py
+++ Experiments/Ship.py
@@ -2,8 +2,9 @@ combustible = 100
 salto_espacial = 5
 while salto_espacial > 0:
     gasto = int(input("¿Cuánta gasolina se gasto en el viaje? "))
-    salto_espacial -= 1
-    combustible -= gasto
+    if combustible > 0:
+        salto_espacial -= 1
+        combustible -= gasto
     if combustible <= 0:
         print("Te haz quedado sin combustible, nave barada")
         break
